befad236a4c50394b188029bee4301a0f72361cc
Daniel Biedma Ramos <info@dbr.io>
2016-10-21 17:32:10 +0100

    last changes, project working

--- src/app/register.js
+++ src/app/register.js
@@ -3,6 +3,6 @@ import ko from 'knockout';
 ko.components.register('router', { require: 'containers/router/router' })
 ko.components.register('default', { require: 'containers/default/default' })
 ko.components.register('product', { require: 'containers/product/product' })
+ko.components.register('bss-nav-bar', { require: 'containers/nav-bar/nav-bar' })
 
 ko.components.register('main-structure', { require: 'components/main-structure/main-structure' })
-ko.components.register('bss-nav-bar', { require: 'components/nav-bar/nav-bar' })
---
+++ src/containers/nav-bar/nav-bar.js
@@ -0,0 +1,64 @@
+import $ from 'jquery';
+import ko from 'knockout';
+import template from 'text!./nav-bar.html';
+
+import { bindActionCreators } from 'redux'
+import {createSelector} from '../../app/utils/reselect'
+import {connect} from "../../app/store/index"
+import {getRouter} from '../../app/reducers/index'
+import routes from '../../app/routes'
+
+import 'ojs/ojknockout'
+import 'ojs/ojdialog'
+import 'ojs/ojtoolbar'
+import 'ojs/ojbutton'
+import 'ojs/ojmenu'
+import 'ojs/ojinputtext'
+import 'ojs/ojoffcanvas'
+
+const mapActionsToDispatch = dispatch => bindActionCreators({}, dispatch)
+
+const mapStateToProps = createSelector(
+    getRouter,
+    (router) => ({ router })
+)
+
+@connect(mapStateToProps, mapActionsToDispatch)
+class viewModel {
+    onInit(params){
+        // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
+        // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
+        // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
+        this.route = 'home';
+        this.appName = 'Knockout, Redux and JET'
+        // Media Queries for repsonsive header
+        var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+        this.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+
+        this.routes = new oj.ArrayTableDataSource(routes, {idAttribute: 'container'});
+        this.selectedItem = ko.observable();
+    }
+
+    menuItemSelect(event, ui) {
+        switch (ui.item.attr("id")) {
+            case "about":
+                $("#aboutDialog").ojDialog("open");
+                break;
+            default:
+        }
+    }
+
+    toggleDrawer () {
+        oj.OffcanvasUtils.toggle({
+            displayMode: 'push',
+            selector: '#offcanvas'
+        });
+    }
+
+    openPopup = (data, event) => {
+        $('#config').ojDialog("open");
+        return true;
+    }
+}
+
+export default { viewModel, template };
